cf37b6784328893ce6e3d82f8cae6fcdd5219551
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-04-26 19:12:25 +0200

    user create function

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.views import LogoutView
-from django.urls import path
-from accounts.views import SubmittableLoginView, SubmittablePasswordChangeView
+from django.urls import path, include
+from accounts.views import SubmittableLoginView, SubmittablePasswordChangeView, signup
+from django.contrib import admin
 
 
 app_name = "accounts"
 urlpatterns = [
+    path('signup/', signup, name="signup"),
     path("login/", SubmittableLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path(
