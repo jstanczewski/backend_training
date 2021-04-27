088ae402aca3cd33f5fab4419a11f9ed321d638e
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-04-27 18:42:01 +0200

    Admin panel edits

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,6 +1,10 @@
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, site
 from viewer.models import Genre, Movie
 
 
-admin.site.register(Genre)
-admin.site.register(Movie)
+class MovieAdmin(ModelAdmin):
+    list_display = ['id', 'title', 'genre']
+
+
+site.register(Genre)
+site.register(Movie, MovieAdmin)
