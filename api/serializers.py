cf0c2cfa23517e66a8bf9512054eafc3441bbb33
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-04-29 20:20:17 +0200

    serializers added

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,17 @@
-from viewer.models import Movie
+from viewer.models import Movie, Genre
 from rest_framework import serializers
 
 
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = '__all__'
+
+
 class MovieSerializer(serializers.ModelSerializer):
+    # genre = GenreSerializer(many=False, read_only=True)
+
     class Meta:
         model = Movie
         fields = "__all__"
+        excluded = ["created"]
